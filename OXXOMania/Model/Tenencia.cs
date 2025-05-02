2e5879e1c22f062b61bd9d27f2a7cd905f30fbfa
gabrielmartineza01571698 <a01571698@tec.mx>
2025-05-01 18:50:13 -0600

    avatar_editable

---
+++ OXXOMania/Model/Tenencia.cs
@@ -0,0 +1,9 @@
+
+namespace OXXOMania.Model {
+    public class Tenencia {
+        public int IdUsuario { get; set; }
+        public int IdObjeto { get; set; }
+        public bool Comprado { get; set; }
+        public bool Equipado { get; set; }
+    }
+}
