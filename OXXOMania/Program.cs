b2fcf8aca4ff0c5c761419c386b289f4b38c26a3
gabrielmartineza01571698 <149699672+gabrielmartineza01571698@users.noreply.github.com>
2025-05-13 15:27:03 -0600

    Update Program.cs

--- OXXOMania/Program.cs
+++ OXXOMania/Program.cs
@@ -1,15 +1,15 @@
+using OXXOMania.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.Add(new ServiceDescriptor(typeof(DataBaseContext), new DataBaseContext()));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -19,7 +19,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages().WithStaticAssets();
 
 app.Run();
