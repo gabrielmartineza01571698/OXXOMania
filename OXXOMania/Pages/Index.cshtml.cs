1a88c9966e79cf0e0aebab1b3f5176f93a9c9d49
Karla Rosas <a00839680@tec.mx>
2025-05-16 13:26:31 -0600

    Cambios a index

--- OXXOMania/Pages/Index.cshtml.cs
+++ OXXOMania/Pages/Index.cshtml.cs
@@ -5,12 +5,10 @@ namespace OXXOMania.Pages;
 
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
-    {
-        _logger = logger;
-    }
+    [BindProperty]
+    public string? user {get; set;}
+    [BindProperty]
+    public int? password {get; set;}
 
     public void OnGet()
     {
