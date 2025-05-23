f630fc5c719ade645710cd56b4241d048b952488
Meminbb <kevinbolanos22@gmail.com>
2025-05-22 22:10:24 -0600

    change pages

--- OXXOMania/Pages/Index.cshtml.cs
+++ OXXOMania/Pages/Index.cshtml.cs
@@ -6,12 +6,20 @@ namespace OXXOMania.Pages;
 public class IndexModel : PageModel
 {
     [BindProperty]
-    public string? user {get; set;}
+    public string? user { get; set; }
     [BindProperty]
-    public int? password {get; set;}
+    public int? password { get; set; }
 
     public void OnGet()
     {
 
+    }
+    
+    public IActionResult OnPost()
+    {
+        // Aquí podrías guardar los datos o hacer alguna validación
+
+        // Redirige a la página Personalizacion
+        return RedirectToPage("/Podium");
     }
 }
