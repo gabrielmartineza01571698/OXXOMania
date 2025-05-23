d80b5a93f033056128051975b817402737e2160c
Meminbb <kevinbolanos22@gmail.com>
2025-05-22 22:20:06 -0600

    pagina mari funcional

--- OXXOMania/Pages/VistaAsesor.cshtml.cs
+++ OXXOMania/Pages/VistaAsesor.cshtml.cs
@@ -1,1 +1,17 @@
-//fef;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OXXOMania.Pages;
+
+public class VistaAsesor : PageModel
+{
+    [BindProperty]
+    public string? user {get; set;}
+    [BindProperty]
+    public int? password {get; set;}
+
+    public void OnGet()
+    {
+
+    }
+}
