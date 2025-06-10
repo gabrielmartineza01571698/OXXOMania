37a71d23dd1541c0c01708cb69c076e24c939132
mangejlu <152338849+mangejlu@users.noreply.github.com>
2025-06-09 23:26:50 -0600

    try catch

--- OXXOMania/Pages/DashboardLider.cshtml.cs
+++ OXXOMania/Pages/DashboardLider.cshtml.cs
@@ -3,12 +3,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Http;
 using System.Text.Json;
 using OXXOMania.Model;
+using System; // Necesario para Exception
 
 namespace OXXOMania.Pages
 {
     public class DashboardLiderModel : PageModel
-    { 
-
+    {
         private readonly IHttpContextAccessor lectorSesion;
         private readonly DataBaseContext db;
 
@@ -19,7 +19,7 @@ namespace OXXOMania.Pages
         }
 
         public int? id_lider { get; set; }
-        public List<Empleado> listaEmpleados { get; set; } = new List<Empleado>();
+        public List<Empleado> listaEmpleados { get; set; } = null;
 
         public void OnGet(int id_usuario)
         {
@@ -29,12 +29,21 @@ namespace OXXOMania.Pages
                 var usr = JsonSerializer.Deserialize<Usuario>(jsonUsr);
                 if (usr != null)
                 {
+                    try
+                    {
+                        // Intenta obtener la lista de empleados desde la base de datos
+                        listaEmpleados = db.AgarrarHorarios(id_usuario);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Aquí puedes registrar el error si tienes un sistema de logging
+                        Console.WriteLine("Error al acceder a la base de datos: " + ex.Message);
 
-                    listaEmpleados = db.AgarrarHorarios(id_usuario);
-
+                        // Dejar listaEmpleados en null indica que falló la conexión a la BD
+                        listaEmpleados = null;
+                    }
                 }
             }
         }
     }
-
 }
