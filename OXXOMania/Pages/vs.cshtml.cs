7cd8d2dd0a58439f528f387e38de5449bb28dc20
gabrielmartineza01571698 <149699672+gabrielmartineza01571698@users.noreply.github.com>
2025-06-09 20:30:24 -0600

    Create vs.cshtml.cs

---
+++ OXXOMania/Pages/vs.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+using OXXOMania.Model;
+
+namespace OXXOMania.Pages
+{
+     public class vjModel : PageModel
+    {
+        public List<Instruccion> Instrucciones { get; set; }
+        public void OnGet()
+        {
+            DataBaseContext db = new DataBaseContext();
+            Instrucciones = db.ObtenerInstrucciones();
+        }
+    }
+
+}
